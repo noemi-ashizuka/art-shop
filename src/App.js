123fef4313a4711a7f7710914a5625d425b95852
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-12 11:38:27 +0900

    changed directory structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
 
 const PotteryPage = () => (
   <div>
