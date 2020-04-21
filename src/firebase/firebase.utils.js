3bd57ca707b25f8c8e35be44cd1cdc9426324022
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-04-21 13:09:07 +0900

    update

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: "AIzaSyA6_k9bCRAL4Vm9OgMLdo9fATEd5P2gJaY",
-  authDomain: "art-sale.firebaseapp.com",
-  databaseURL: "https://art-sale.firebaseio.com",
-  projectId: "art-sale",
-  storageBucket: "art-sale.appspot.com",
-  messagingSenderId: "823379585085",
-  appId: "1:823379585085:web:95a182608d1ea5a73f2254"
-}
+
 
 firebase.initializeApp(config);
 
